89bf83250ef43e65262d7f892f6b33eb476a966d
Ret4rd <40753054+Ret4rd@users.noreply.github.com>
2020-04-25 20:25:30 +0300

    Add files via upload

---
+++ lab11/lab11/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Complex com1 = new Complex(4, 3);
+                Complex com2 = new Complex(0, 0);
+                Console.WriteLine("Первое комплексное число");
+                Console.WriteLine(com1.ToString());
+                Console.WriteLine("Второе комплексное число");
+                Console.WriteLine(com2.ToString());
+                Console.WriteLine("Сложение");
+                Console.WriteLine((com1 + com2).ToString());
+                Console.WriteLine("Разность");
+                Console.WriteLine((com1 - com2).ToString());
+                Console.WriteLine("Умножение");
+                Console.WriteLine((com1 * com2).ToString());
+                Console.WriteLine("Деление");
+                Console.WriteLine((com1 / com2).ToString());
+                Console.WriteLine("Модуль первого числа");
+                Console.WriteLine(Complex.Module(com1));
+                Console.WriteLine("Возведение первого числа в степень 4");
+                Console.WriteLine(Complex.Pow(com1, 4).ToString());
+                Console.WriteLine("Возведение первого числа в степень 1/2");
+                var sq = Complex.Sqrt(com1, 2);
+                for (int i = 0; i < sq.Length; i++)
+                    Console.WriteLine(sq[i]);
+                
+                Vector<double> vec1 = new Vector<double>(new double[3] { 1, 2, 3 });
+                Vector<double> vec2 = new Vector<double>(new double[3] { 1, 5, 2 });
+                Console.WriteLine("Первый вектор");
+                vec1.Print();
+                Console.WriteLine("Второй вектор");
+                vec2.Print();
+                Console.WriteLine("Сложение");
+                (vec1 + vec2).Print();
+                Console.WriteLine("Разность");
+                (vec1 - vec2).Print();
+                Console.WriteLine("Умножение на 2");
+                (vec1 * 2).Print();
+                Console.WriteLine("Модуль первого вектора");
+                Console.WriteLine(Vector<double>.Module(vec1));
+                Console.WriteLine("Скалярное произведение");
+                Console.WriteLine(Vector<double>.Scalar(vec1, vec2));
+                Console.WriteLine("Ортогонализация 1 и 2 вектора");
+                var vecArr = new Vector<double>[2] { vec1, vec2 };
+                var res = Vector<double>.Ortogonalization(vecArr);
+                for (int i = 0; i < res.Length; i++)
+                {
+                    res[i].Print();
+                }
+                var doubleV = (double[])vec1;
+                for (int i = 0; i < doubleV.Length; i++)
+                    Console.WriteLine(doubleV[i]);
+                var arrVec = (Vector<double>)doubleV;
+                arrVec.Print();
+
+                Vector<Complex> vecCom1 = new Vector<Complex>(new Complex[2] { com1, com2 });
+                Vector<Complex> vecCom2 = new Vector<Complex>(new Complex[2] { com2, com1 });
+                Console.WriteLine("Первый вектор");
+                vecCom1.Print();
+                Console.WriteLine("Второй вектор");
+                vecCom2.Print();
+                Console.WriteLine("Сложение");
+                (vecCom1 + vecCom2).Print();
+                Console.WriteLine("Разность");
+                (vecCom1 - vecCom2).Print();
+                Console.WriteLine("Модуль первого вектора");
+                Console.WriteLine(Vector<Complex>.Module(vecCom1));
+                Console.WriteLine("Скалярное произведение");
+                Console.WriteLine(Vector<Complex>.Scalar(vecCom1, vecCom2));
+                Console.WriteLine("Ортогонализация 1 и 2 вектора");
+                Console.WriteLine(Vector<Complex>.Scalar(vecCom1, vecCom2));
+                Console.WriteLine("Ортогонализация 1 и 2 вектора");
+                var vecComArr = new Vector<Complex>[2] { vecCom1, vecCom2 };
+                var resCom = Vector<Complex>.Ortogonalization(vecComArr);
+                for (int i = 0; i < resCom.Length; i++)
+                {
+                    resCom[i].Print();
+                }
+                var comV = (Complex[])vecCom1;
+                for(int i = 0; i < comV.Length; i++)
+                    Console.WriteLine(comV[i]);
+                var comVec = (Vector<Complex>)comV;
+                comVec.Print();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
---
+++ lab11/lab11/Vector.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11
+{
+    class Vector<T> : IComparable, IComparable<Vector<T>> where T : IComparable, new()
+    {
+        private T[] vector;
+
+        public Vector()
+        {
+            this.vector = new T[1];
+        }
+
+        public Vector(T[] vec)
+        {
+            if (vec.Length <= 0)
+                throw new Exception("Длина вектора меньше нуля");
+            this.vector = new T[vec.Length];
+            for(int i = 0; i < vec.Length; i++)
+            {
+                this.vector[i] = vec[i]; 
+            }
+        }
+
+        public Vector(double size)
+        {
+            if (size <= 0)
+                throw new Exception("Длина меньше нуля");
+            this.vector = new T[(int)size];
+        }
+
+        public static Vector<T> operator +(Vector<T> vec1, Vector<T> vec2)
+        {
+            return Sum(vec1, vec2);
+        }
+
+        public static Vector<T> Sum(Vector<T> vec1, Vector<T> vec2)
+        {
+            if (vec1.vector.Length != vec2.vector.Length)
+                throw new Exception("Разные размеры векторов");
+            Vector<T> res = new Vector<T>(vec1.vector.Length);
+            for(int i = 0; i < vec1.vector.Length; i++)
+            {
+                res.vector[i] = (dynamic)vec1.vector[i] + vec2.vector[i];
+            }
+            return res;
+        }
+
+        public static Vector<T> operator -(Vector<T> vec1, Vector<T> vec2)
+        {
+            return Sub(vec1, vec2);
+        }
+        public static Vector<T> Sub(Vector<T> vec1, Vector<T> vec2)
+        {
+            if (vec1.vector.Length != vec2.vector.Length)
+                throw new Exception("Разные размеры векторов");
+            Vector<T> res = new Vector<T>(vec1.vector.Length);
+            for (int i = 0; i < vec1.vector.Length; i++)
+            {
+                res.vector[i] = (dynamic)vec1.vector[i] - vec2.vector[i];
+            }
+            return res;
+        }
+
+        public static Vector<T> operator *(Vector<T> vec1, T num)
+        {
+            return Mult(vec1, num);
+        }
+        public static Vector<T> Mult(Vector<T> vec1, T num)
+        {
+            Vector<T> res = new Vector<T>(vec1.vector.Length);
+            for (int i = 0; i < vec1.vector.Length; i++)
+            {
+                res.vector[i] = (dynamic)vec1.vector[i] * num;
+            }
+            return res;
+        }
+
+        public static double Module(Vector<T> vec)
+        {
+            double res = 0.0;
+            //Vector<T> res = new Vector<T>(vec.vector.Length);
+            if (vec.vector[0] is Complex)
+            {
+                //Complex[] res = new Complex[1];
+                for (int i = 0; i < vec.vector.Length; i++)
+                {
+                    res = res + Complex.Module((dynamic)vec.vector[i]) * Complex.Module((dynamic)vec.vector[i]);
+                }
+                
+            }
+            else
+            {
+                
+                for (int i = 0; i < vec.vector.Length; i++)
+                {
+                    res = res + (dynamic)vec.vector[i] * vec.vector[i];
+                }
+                //res = Math.Sqrt((dynamic)res);
+                //return res;
+            }
+            res = Math.Sqrt((dynamic)res);
+            return res;
+
+        }
+
+        public static T Scalar(Vector<T> vec1, Vector<T> vec2)
+        {
+            if (vec1.vector.Length != vec2.vector.Length)
+                throw new Exception("Разные размеры векторов");
+            T res = new T();
+            for (int i = 0; i < vec1.vector.Length; i++)
+            {
+                res = res + (dynamic)vec1.vector[i] * vec2.vector[i];
+            }
+            return res;
+        }
+
+        //public static Vector<T>[] Ortho(Vector<T>[] vec)
+        //{
+
+        //    for(int i = 0; i < vec.Length; i++)
+        //    {
+
+        //    }
+        //    return 
+        //}
+
+        private static T[] ZeroArr(Vector<T> vec)
+        {
+            return Enumerable.Repeat(new T(), vec.vector.Length).ToArray();
+        }
+
+        public static Vector<T>[] Ortogonalization(Vector<T>[] vec)
+        {
+            var b = new Vector<T>[vec.Length];
+            b[0] = vec[0];
+            for (int i = 1; i < vec.Length; i++)
+            {
+                var zeroArr = ZeroArr(vec[i]);
+                var tmpVector = new Vector<T>(zeroArr);
+                var sumVector = new Vector<T>(zeroArr);
+                for (int j = 0; j < i; j++)
+                {
+                    T k = (dynamic)(Scalar(vec[i], b[j])) / (Scalar(b[j], b[j]));
+                    sumVector -= b[j] * k;
+                }
+                b[i] = Sum(vec[i], sumVector);
+                sumVector = tmpVector;
+            }
+            return b;
+        }
+
+        public void Print()
+        {
+            foreach(T num in this.vector)
+            {
+                Console.Write(num.ToString() + " ");
+            }
+            Console.WriteLine();
+            return;
+        }
+
+
+        public static implicit operator T[] (Vector<T> A) => (T[])A.vector.Clone();
+
+        public static implicit operator Vector<T>(T[] Arr) => new Vector<T>(Arr);
+
+        public int CompareTo(Vector<T> vec)
+        {
+            return Module(this).CompareTo(Module(vec));
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+
+            if (!(obj is Vector<T>))
+            {
+                throw new ArgumentException("Это не вектор");
+            }
+
+            var res = obj as Vector<T>;
+            return CompareTo(res);
+        }
+    }
+}
