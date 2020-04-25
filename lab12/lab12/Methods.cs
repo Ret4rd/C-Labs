5efd5a0d66b4df7c091040d671c60d48aacbcff0
Ret4rd <40753054+Ret4rd@users.noreply.github.com>
2020-04-25 23:59:59 +0300

    Add files via upload

---
+++ lab12/lab12/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06
+{
+    class Methods
+    {
+        public static List<string> Sort(string[] str)
+        {
+            var wordsList = new List<string>(str);
+            wordsList.Sort();
+            return wordsList;
+        }
+
+        public static string LastWord(List<string> wordsList)
+        {
+            var res = new StringBuilder();
+            if (wordsList.Count == 0)
+                throw new Exception("Нет слов");
+            foreach(string str in wordsList)
+            {
+                res.Append(str.Last());
+                //res.Insert(res.Length, str.Last()); //+= str.Last();
+            }
+            return res.ToString();
+        }
+    }
+}
